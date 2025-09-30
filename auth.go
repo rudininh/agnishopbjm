e6a09061d8602dab166db5149d7d9968089f5eac
rudininh <sorows22@gmail.com>
2025-09-30 16:29:09 +0800

    kwkwk

--- auth.go
+++ auth.go
@@ -1,33 +1,25 @@
 package main
 
 import (
-    "crypto/hmac"
-    "crypto/sha256"
-    "encoding/hex"
-    "fmt"
-    "time"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"time"
 )
 
 func main() {
-    partnerID := "1189715"
-    partnerKey := "ISI_PARTNER_KEY"
-    redirect := "http://localhost.com"
-
-    path := "/api/v2/shop/auth_partner"
-    timestamp := time.Now().Unix()
-
-    // signature
-    base := fmt.Sprintf("%s%s%d", partnerID, path, timestamp)
-    mac := hmac.New(sha256.New, []byte(partnerKey))
-    mac.Write([]byte(base))
-    sign := hex.EncodeToString(mac.Sum(nil))
-
-    // URL untuk authorize
-    url := fmt.Sprintf(
-        "https://open.sandbox.test-stable.shopee.com/auth?auth_type=seller&partner_id=1189715&redirect_uri=http%3A%2F%2Flocalhost.com&response_type=code",
-        path, partnerID, timestamp, sign, redirect,
-    )
-
-    fmt.Println("Buka URL ini di browser:")
-    fmt.Println(url)
+	timest := strconv.FormatInt(time.Now().Unix(), 10)
+	host := "https://partner.test.shopeemobile.com"
+	path := "/api/v2/shop/auth_partner"
+	redirectUrl := "https://www.baidu.com/"
+	partnerId := strconv.Itoa(2006566)
+	partnerKey := "1391fd986fe8ec7569bebed75b0c33ee35eb5a305bed7038657a5cd5f75b1c88"
+	baseString := fmt.Sprintf("%s%s%s", partnerId, path, timest)
+	h := hmac.New(sha256.New, []byte(partnerKey))
+	h.Write([]byte(baseString))
+	sign := hex.EncodeToString(h.Sum(nil))
+	url := fmt.Sprintf(host+path+"?partner_id=%s&timestamp=%s&sign=%s&redirect=%s", partnerId, timest, sign, redirectUrl)
+	fmt.Println(url)
 }
