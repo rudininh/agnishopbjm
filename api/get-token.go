362aa4de34bfb60070b8a73e56e4444991aa80f1
rudininh <sorows22@gmail.com>
2025-10-16 16:21:54 +0800

    tes lagi

---
+++ api/get-token.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const (
+	PartnerID  = 2013107
+	PartnerKey = "shpk5a76537146704b44656a4a6f4f685271464b596b71557353544a71436465"
+	Host       = "https://partner.shopeemobile.com"
+)
+
+func generateSign(baseString, key string) string {
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(baseString))
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Bisa diambil dari parameter kalau mau dinamis
+	code := "4c636771635744754d736b5251547077"
+	shopID := 380921117
+	timestamp := time.Now().Unix()
+	path := "/api/v2/auth/token/get"
+
+	baseString := fmt.Sprintf("%d%s%d", PartnerID, path, timestamp)
+	sign := generateSign(baseString, PartnerKey)
+
+	url := fmt.Sprintf("%s%s?partner_id=%d&timestamp=%d&sign=%s", Host, path, PartnerID, timestamp, sign)
+
+	body := map[string]interface{}{
+		"code":       code,
+		"shop_id":    shopID,
+		"partner_id": PartnerID,
+	}
+	jsonBody, _ := json.Marshal(body)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Request gagal: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, _ := io.ReadAll(resp.Body)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBody)
+}
