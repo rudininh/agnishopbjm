fdad51941f910476ee344470aa8e89f04dc4ab02
rudininh <sorows22@gmail.com>
2025-10-16 15:10:48 +0800

    ubah login js

--- api/login.js
+++ api/login.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         'Set-Cookie',
         `admin_logged_in=true; Path=/; HttpOnly; Max-Age=3600;`
       );
-      return res.status(200).json({ success: true, redirect: '/dashboard.html' });
+      return res.status(200).json({ success: true, redirect: 'dashboard.html' });
     } else {
       return res.status(401).json({ success: false, message: 'Username atau password salah!' });
     }
