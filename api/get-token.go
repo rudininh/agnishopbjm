e5e594d03a67441f8a955aa3218d1329e50a255c
Rudini Nor Habibi <sorows22@gmail.com>
2025-10-17 06:45:23 +0800

    get code

--- api/get-token.go
+++ api/get-token.go
@@ -25,7 +25,7 @@ func generateSign(baseString, key string) string {
 }
 
 func main() {
-	code := "6268485a6d5a6b50674d64717a76514f" // kode hasil callback terbaru
+	code := "6b687461727949627864627747756178" // kode hasil callback terbaru
 	shopID := 380921117                        // shop_id hasil callback
 	timestamp := time.Now().Unix()
 	path := "/api/v2/auth/token/get"
