526b0e4fb6902d55763f68f810c551d5cb343910
rudininh <sorows22@gmail.com>
2025-10-16 14:59:49 +0800

    hapus yang tidak penting

--- config.go
+++
@@ -1,8 +0,0 @@
-package main
-
-const (
-	PartnerID   = 2013107
-	PartnerKey  = "shpk5a76537146704b44656a4a6f4f685271464b596b71557353544a71436465" // jaga baik2 jangan bocor
-	RedirectURL = "https://funny-haupia-0efca3.netlify.app/callback"
-	BaseURL     = "https://partner.shopeemobile.com/api/v2"
-)
