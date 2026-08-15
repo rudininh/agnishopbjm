86cdd68ce221a76d92d97034fb8f5ec3279d2dd2
Rudini Nor Habibi <sorows22@gmail.com>
2026-08-16 07:36:02 +0800

    test: make Gitashop workbook fixture self-contained

--- backend/tests/Feature/ShopeeMassUploadWorkbookManifestTest.php
+++ backend/tests/Feature/ShopeeMassUploadWorkbookManifestTest.php
@@ -14,10 +14,39 @@ class ShopeeMassUploadWorkbookManifestTest extends TestCase
     use RefreshDatabase;
 
     private string $relativeDirectory = 'import-marketplace/generated/workbook-manifest-test';
+    private string $templateDirectory = 'import-marketplace/shopee-gita';
+    private array $templateBackups = [];
+    private bool $templateDirectoryExisted = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $directory = storage_path('app/'.$this->templateDirectory);
+        $this->templateDirectoryExisted = File::isDirectory($directory);
+        File::ensureDirectoryExists($directory);
+
+        foreach ($this->templateFilenames() as $filename) {
+            $path = $directory.'/'.$filename;
+            $this->templateBackups[$path] = File::exists($path) ? File::get($path) : null;
+            $this->writeWorkbookTemplate($path);
+        }
+    }
 
     protected function tearDown(): void
     {
         File::deleteDirectory(storage_path('app/'.$this->relativeDirectory));
+
+        foreach ($this->templateBackups as $path => $contents) {
+            if ($contents === null) {
+                File::delete($path);
+            } else {
+                File::put($path, $contents);
+            }
+        }
+        if (! $this->templateDirectoryExisted) {
+            File::deleteDirectory(storage_path('app/'.$this->templateDirectory));
+        }
 
         parent::tearDown();
     }
@@ -97,5 +126,44 @@ class ShopeeMassUploadWorkbookManifestTest extends TestCase
         $xpath->registerNamespace('x', 'http://schemas.openxmlformats.org/spreadsheetml/2006/main');
 
         return collect($xpath->query('//x:c'))->map(fn ($node) => $node->textContent)->all();
+    }
+
+    private function templateFilenames(): array
+    {
+        return [
+            'mass_update_basic_info.xlsx',
+            'mass_update_sales_info.xlsx',
+            'mass_update_media_info.xlsx',
+            'mass_update_shipping_info.xlsx',
+            'mass_update_dts_info.xlsx',
+            'mass_republish_items.xlsx',
+        ];
+    }
+
+    private function writeWorkbookTemplate(string $path): void
+    {
+        $zip = new ZipArchive();
+        $this->assertTrue($zip->open($path, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true);
+        $zip->addFromString('[Content_Types].xml', <<<'XML'
+<?xml version='1.0' encoding='UTF-8'?>
+<Types xmlns='http://schemas.openxmlformats.org/package/2006/content-types'><Default Extension='rels' ContentType='application/vnd.openxmlformats-package.relationships+xml'/><Default Extension='xml' ContentType='application/xml'/><Override PartName='/xl/workbook.xml' ContentType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml'/><Override PartName='/xl/worksheets/sheet1.xml' ContentType='application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml'/></Types>
+XML);
+        $zip->addFromString('_rels/.rels', <<<'XML'
+<?xml version='1.0' encoding='UTF-8'?>
+<Relationships xmlns='http://schemas.openxmlformats.org/package/2006/relationships'><Relationship Id='rId1' Type='http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument' Target='xl/workbook.xml'/></Relationships>
+XML);
+        $zip->addFromString('xl/workbook.xml', <<<'XML'
+<?xml version='1.0' encoding='UTF-8'?>
+<workbook xmlns='http://schemas.openxmlformats.org/spreadsheetml/2006/main' xmlns:r='http://schemas.openxmlformats.org/officeDocument/2006/relationships'><sheets><sheet name='Sheet1' sheetId='1' r:id='rId1'/></sheets></workbook>
+XML);
+        $zip->addFromString('xl/_rels/workbook.xml.rels', <<<'XML'
+<?xml version='1.0' encoding='UTF-8'?>
+<Relationships xmlns='http://schemas.openxmlformats.org/package/2006/relationships'><Relationship Id='rId1' Type='http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet' Target='worksheets/sheet1.xml'/></Relationships>
+XML);
+        $zip->addFromString('xl/worksheets/sheet1.xml', <<<'XML'
+<?xml version='1.0' encoding='UTF-8'?>
+<worksheet xmlns='http://schemas.openxmlformats.org/spreadsheetml/2006/main'><sheetData><row r='7'><c r='A7' t='inlineStr'><is><t>58112069183</t></is></c><c r='C7' t='inlineStr'><is><t>356049039888</t></is></c><c r='Q7' t='inlineStr'><is><t>Soft blue</t></is></c></row></sheetData></worksheet>
+XML);
+        $zip->close();
     }
 }
